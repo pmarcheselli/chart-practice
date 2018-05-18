cadb4f914dd325745ccc5a0f6836e8cedebb505d
Pmarch <pmarcheselli@gmail.com>
2018-05-17 20:41:23 -0700

    created timeline

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("let's build some charts!")
 
       // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart','bar']});
+      google.charts.load('current', {'packages':['corechart','bar','timeline']});
 
 
       // ---- Only have to do this once!
@@ -12,12 +12,37 @@ console.log("let's build some charts!")
       // 4. Be sure all callback function use different divs to draw in. Add a new div to HTML for each chart.
 
       // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawDonationChart);
+      // google.charts.setOnLoadCallback(drawDonationChart);
+      google.charts.setOnLoadCallback(drawStudyTimeline);
+      google.charts.setOnLoadCallback(drawStudyBar);
 
 
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
       // draws it.
+
+      function drawStudyTimeline(){
+        var container = document.getElementById('study-timeline');
+        var chart = new google.visualization.Timeline(container);
+        var dataTable = new google.visualization.DataTable();
+
+        dataTable.addColumn({ type: 'string', id: 'Phase of Study' });
+        dataTable.addColumn({ type: 'date', id: 'Start' });
+        dataTable.addColumn({ type: 'date', id: 'End' });
+        dataTable.addRows([
+          ['General Ed', new Date(2018, 8, 1), new Date(2019, 4, 1)],
+          ['Choose Mjor',      new Date(2019, 5, 1),  new Date(2019, 7, 1)],
+          ['2 Mjr Class',  new Date(2019, 8, 1),  new Date(2020, 4, 1)],
+          ['12 Mjr Class',  new Date(2020, 8, 1),  new Date(2022, 4, 1)]
+        ]);
+
+        chart.draw(dataTable);
+      }
+
+      function drawStudyBar(){
+        
+      }
+
       function drawDonationChart() {
         // #donation-chart is the container
           var data = google.visualization.arrayToDataTable([
@@ -42,8 +67,7 @@ console.log("let's build some charts!")
           width: 800,
           legend: 'top'
         };
-        
-        
+
         // hex codes #8AD1C2, #9F8AD1, #D18A99, #BCD18A
 
         var chart = new google.visualization.PieChart(document.getElementById('donation-chart'));
